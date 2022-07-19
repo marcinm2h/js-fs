51736b5eb1b69af12428333629afe61f9b54dad1
Marcin Moch <marcio.moch@gmail.com>
2022-07-19 15:45:31 +0200

    feat: express

--- src/express.js
+++ src/express.js
@@ -1,53 +1,78 @@
-const http = require('http');
+const http = require('node:http');
 
-const server = http.createServer((req, res) => {
-  console.log('onReq', req.method, req.url);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(
-    JSON.stringify({
-      data: 'Hello world',
-    }),
-  );
-  res.end();
-});
+// TODO: middlewares, url matching eg. /a/*/b -> /a/b/b ...
+// TODO: GET, POST, DELETE, HEAD, PUT, OPTIONS, CONNECT, TRACE
+class Router {
+  _state = {
+    get: [],
+  };
+  use(router) {
+    this._state.get = [...this._state.get, ...router._state.get];
+  }
+  getHandler(method, url) {
+    const item = this._state[method.toLowerCase()].find(
+      (item) => item.url === url,
+    );
+    return item ? item.handler : null;
+  }
+  get(url, handler) {
+    this._state.get.push({ url, handler });
+  }
+}
 
-[
-  'checkContinue',
-  'checkExpectation',
-  'clientError',
-  'close',
-  'connect',
-  'connection', // TCP stream is established
-  'error',
-  'listening', // no args
-  'request', // req, res
-  'upgrade',
-].forEach((event) => {
-  return;
-  server.on(event, (...args) => {
-    console.log(event);
-  });
-});
+const responseSendDecorator = (res) => {
+  return {
+    ...res,
+    send: (val) => {
+      res.writeHead(200);
+      res.end(val);
+    },
+  };
+};
+const express = () => {
+  const router = new Router();
+  const app = {
+    get: router.get.bind(router),
+    use: (nextRouter) => {
+      router.use(nextRouter);
+    },
+    listen: (port, onListen) => {
+      const server = http.createServer((req, res) => {
+        console.log('onReq', req.method, req.url);
+        const handler = router.getHandler(req.method, req.url);
+        if (handler) {
+          return handler(req, responseSendDecorator(res));
+        }
+        // 404
+      });
+      server.listen(port, null, onListen);
+      return app;
+    },
+  };
+  return app;
+};
+express.Router = () => new Router();
 
-server.on('request', (req, res) => {
-  console.log('res.writableEnded', res.writableEnded);
-});
-
-server.listen(process.env.PORT || 9900, null, () => {
-  console.log(`server started at http://localhost:${server.address().port}`);
-});
-
-//FIXME: stub -> implementation
-const express = () => ({
-  get: () => {},
-  listen: () => {},
-});
 const app = express();
 app.get('/', (req, res) => {
   res.send('hello');
-  // res.json({ hello: 'hello'})
 });
+
+const r = express.Router();
+r.get('/new', (req, res) => {
+  res.send('new');
+});
+
+const r2 = express.Router();
+r2.get('/r2', (req, res) => {
+  res.send('r2');
+});
+
+r.use(r2);
+
+app.use(r);
+
 const port = process.env.PORT || 3322;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
