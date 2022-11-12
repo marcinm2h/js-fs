6ebad7f6c178faca850e454aba5df5b9ed2660bc
Marcin Moch <marcio.moch@gmail.com>
2022-11-13 00:59:47 +0100

    feat: async logic in ssr app

--- apps/react-ssr-app/src/pages/Home.jsx
+++ apps/react-ssr-app/src/pages/Home.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+const delay = (t = 500) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(), t);
+  });
+
+const api = {
+  getUser: async () => {
+    await delay();
+    return {
+      name: 'mm',
+    };
+  },
+};
+
 export default function Home() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    let canceled = false;
+    api.getUser().then((user) => {
+      if (canceled) {
+        return;
+      }
+      setUser(user);
+    });
+    return () => {
+      canceled = true;
+    };
+  }, []);
+
   return (
     <>
       <h1>Home</h1>
+      <p>user: {user?.name}</p>
       <Link to="/about">About</Link>
     </>
   );
